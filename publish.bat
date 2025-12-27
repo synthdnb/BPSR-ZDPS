381ba85879f216bb9fcb4332bddcc76b1c67eb97
Goomii <5684215+GoomiiV2@users.noreply.github.com>
2025-12-27 10:38:50 +0000

    Added github action to compile

--- publish.bat
+++ publish.bat
@@ -1,4 +1,3 @@
-
-dotnet publish "BPSR-ZDPS/BPSR-ZDPS.csproj" -r win-x64 -c Release -o ./publish /p:PublishSingleFile=true /p:PublishTrimmed=false /p:TrimMode=Link /p:IncludeAllContentForSelfExtract=false --self-contained false
+dotnet publish "BPSR-ZDPS/BPSR-ZDPS.csproj" -r win-x64 -c Release -o ./publish /p:PublishSingleFile=true /p:PublishTrimmed=false /p:TrimMode=Link /p:IncludeAllContentForSelfExtract=false /p:DebugType=None /p:DebugSymbols=false --self-contained false
 move "publish\BPSR-ZDPS.exe" "publish\BPSR-ZDPS.exe"
 copy "BPSR-ZDPS\Data" "publish\Data"
